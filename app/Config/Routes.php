ba3d05880be2a5836c6317cdf5995908ab7f3173
CodeDev <70334654+ifNicolas@users.noreply.github.com>
2022-09-02 00:44:55 -0400

    reload
    reload

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -34,9 +34,9 @@ $routes->set404Override();
  */
 
 // We get a performance increase by specifying the default
-// route since we don't have to scan directories. 
+// route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('/', 'pricipal::index');
+
 /*
  * --------------------------------------------------------------------
  * Additional Routing
