1493d4bbb109b56542d88bc206d345aafd347ed3
CodeDev <70334654+ifNicolas@users.noreply.github.com>
2022-09-02 10:22:03 -0400

    actualizacion vista

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -5,11 +5,10 @@ namespace App\Controllers;
 class Home extends BaseController
 {
     public function index()
-    {   
+    {       
+        $estructura=view('headfoot/header').view('inicio').view('headfoot/footer');
 
-        echo view ('headfoot/header');
-
-        echo view ('headfoot/footer');
+        return $estructura;
     }
 
 }
--- app/Views/headfoot/footer.php
+++ app/Views/headfoot/footer.php
@@ -9,17 +9,11 @@
       Diseñado por<a href="index.html"> Escuela Felipe Cubillos</a>
     </div>
   </div>
-  <div class="social-links text-center text-md-right pt-3 pt-md-0">
-    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-  </div>
+  
 </div>
 
 <!-- preloader /style.css/preloader -->
-<div id="preloader"> </div>
+<!-- <div id="preloader"> </div> -->
 <!-- botón que nos redirige hacia el principio de la pagina -->
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 </footer>
