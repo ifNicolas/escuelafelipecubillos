76288f7f054b68927ede60cc976aa8ff4c5b13e8
CodeDev <70334654+ifNicolas@users.noreply.github.com>
2022-09-02 10:27:21 -0400

    a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -36,8 +36,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
-$routes->get('inicio', 'principal::inicio');
-
+//$routes->get('inicio', 'principal::inicio');
+$routes->get('profesores', 'principal::profesores');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
--- app/Views/headfoot/footer.php
+++ app/Views/headfoot/footer.php
@@ -13,7 +13,7 @@
 </div>
 
 <!-- preloader /style.css/preloader -->
-<!-- <div id="preloader"> </div> -->
+<div id="preloader"> </div>
 <!-- botón que nos redirige hacia el principio de la pagina -->
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 </footer>
