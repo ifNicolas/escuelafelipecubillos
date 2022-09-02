30ab981465ab2676bded3fd88e2d631febc695da
CodeDev <70334654+ifNicolas@users.noreply.github.com>
2022-09-02 03:04:22 -0400

    Update footer.php

--- app/Views/headfoot/footer.php
+++ app/Views/headfoot/footer.php
@@ -26,19 +26,19 @@
 <!-- End Footer -->
 <!-- llamados -->
 <!-- Vendor JS Files -->
-<script src="<?php echo base_url()?>assets/vendor/purecounter/purecounter.js"></script>
-<script src="<?php echo base_url()?>assets/vendor/aos/aos.js"></script>
-<script src="<?php echo base_url()?>assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="<?php echo base_url()?>assets/vendor/swiper/swiper-bundle.min.js"></script>
-<script href="<?php echo base_url()?>assets/vendor/fontawesome/js/all.js"></script>
+<script src="<?php echo base_url('assets/vendor/purecounter/purecounter.js')?>"></script>
+<script src="<?php echo base_url('assets/vendor/aos/aos.')?>js"></script>
+<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')?>"></script>
+<script src="<?php echo base_url('assets/vendor/swiper/swiper-bundle.min.js')?>"></script>
+<script href="<?php echo base_url('assets/vendor/fontawesome/js/all.js')?>"></script>
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v13.0" nonce="YZIYWOmM"></script>
 <!-- sdk facebook -->
 <!-- Template Main JS File -->
-<script src="<?php echo base_url()?>assets/js/main.js"></script>
+<script src="<?php echo base_url('assets/js/main.js')?>"></script>
 <!-- custom js -->
-<script src="<?php echo base_url()?>assets/js/js-custom/custom.js"></script>
-<script src="<?php echo base_url()?>assets/js/js-custom/jquery-3.6.0.js"></script>
+<script src="<?php echo base_url('assets/js/js-custom/custom.js')?>"></script>
+<script src="<?php echo base_url('assets/js/js-custom/jquery-3.6.0.js')?>"></script>
 <!-- .custom js -->
 </body>
 
